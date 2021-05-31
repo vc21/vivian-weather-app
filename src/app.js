f7ed90d8adee0e745fa733bc99bcea7601d2e2ab
vc21 <82514334+vc21@users.noreply.github.com>
2021-05-31 19:31:22 +0100

    Showing current date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+function formatDate(now) {
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let now = new Date();
+let today = document.querySelector("#today-date");
+today.innerHTML = formatDate(now);
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
